0a78ce1883d379d5171fbfc1d8d89694dabd0d88
PeckLiu2017 <xiaoyang20170204@gmail.com>
2018-01-05 22:53:17 +0800

    outputArrayToString

---
+++ array/outputArrayToString.js
@@ -0,0 +1,6 @@
+//
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+console.log(numbers.toString()); // 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+var numbersString = numbers.join('-');
+console.log(numbersString); // 1-2-3-4-5-6-7-8-9-10-11-12-13-14-15
